da66bf39e55ce7e16ff88db09593e5c7e0245712
ayushsoni11 <soniayush6441@gmail.com>
2024-06-15 00:21:48 +0530

    Add UserAuth2.0 in front-end projex

---
+++ UserAuth2.0/server.js
@@ -0,0 +1,30 @@
+// backend/server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+
+// Connect to MongoDB
+mongoose.connect("mongodb+srv://ayushsoni:ayush4521@cluster1.6cn2qee.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('MongoDB connected');
+}).catch((err) => {
+    console.error('MongoDB connection error:', err.message);
+});
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../frontend-projex')));
+
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/team', require('./routes/team'));  // Add this line
+
+const PORT = process.env.PORT || 5500;
+
+app.listen(5500, () => {
+    console.log(`Server running on port ${PORT}`);
+});
